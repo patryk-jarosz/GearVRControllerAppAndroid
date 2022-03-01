553ade642bcde936484d19da4f0312cc110f3060
patryk-jarosz <patryksmail@gmail.com>
2022-02-28 21:12:02 -0600

    Updated app icon

--- app/src/main/java/com/app/gearvrcontrollerapp/MainActivity2.java
+++ app/src/main/java/com/app/gearvrcontrollerapp/MainActivity2.java
@@ -1,6 +1,7 @@
 package com.app.gearvrcontrollerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,7 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        final Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 }
