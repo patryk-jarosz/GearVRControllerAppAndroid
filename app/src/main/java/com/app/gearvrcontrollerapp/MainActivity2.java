ea2d1dc779f0b97804e2b7f719997624dee1e859
patryk-jarosz <patryksmail@gmail.com>
2022-03-05 12:53:50 -0600

    Changed toolbar transition math

--- app/src/main/java/com/app/gearvrcontrollerapp/MainActivity2.java
+++ app/src/main/java/com/app/gearvrcontrollerapp/MainActivity2.java
@@ -32,7 +32,23 @@ public class MainActivity2 extends AppCompatActivity {
                 float mPercentageCollapsed = (float)Math.abs(verticalOffset) / (appBarLayout.getTotalScrollRange()/2);
                 float mPercentageExpanded = 1f - mPercentageCollapsed;
                 mLinearTop.animate().alpha(mPercentageExpanded).setDuration(0).start();
-                mTxtToolbar.animate().alpha(mPercentageCollapsed).setDuration(0).start();
+//                mTxtToolbar.animate().alpha(mPercentageCollapsed).setDuration(0).start();
+
+                float mCalcMini = mPercentageCollapsed;
+
+                //  .7f -> 0f
+                //  1f  -> 1f
+
+                if(mPercentageCollapsed >= .7f){
+                    //.3*x=.5
+                    //.3x=.5
+                    //1.67
+                }
+
+                if(mCalcMini <= 0){
+                    mCalcMini = 0;
+                }
+                mTxtToolbar.animate().alpha(mCalcMini).setDuration(0).start();
 
                 if(Math.abs(verticalOffset)-appBarLayout.getTotalScrollRange() == 0) {
                     //  Collapsed
